8e7b44316c4c7d1277084e63b18556f37ebc46be
Ryo-Shigematsu <sgmtryo1211@gmail.com>
2026-05-17 16:01:58 +0900

    Add Spring Boot project skeleton with Maven

---
+++ unit-testing-principles-practice/src/main/java/com/example/unit_testing_principles_practice/UnitTestingPrinciplesPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.unit_testing_principles_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingPrinciplesPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingPrinciplesPracticeApplication.class, args);
+	}
+
+}
---
+++ unit-testing-principles-practice/src/test/java/com/example/unit_testing_principles_practice/UnitTestingPrinciplesPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unit_testing_principles_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingPrinciplesPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
